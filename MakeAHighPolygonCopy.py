c7bbc13bf6061059287a81dcef2ad7b8ac6296ce
darthperry <yutengzhang87@gmail.com>
2017-04-20 13:21:20 -0400

    Update MakeAHighPolygonCopy.py

--- MakeAHighPolygonCopy.py
+++ MakeAHighPolygonCopy.py
@@ -1,5 +1,6 @@
 origal = rt.selection[0]
 rt.execute("maxOps.cloneNodes $ cloneType:#copy newNodes:&nnl select nnl")
+# have to use Maxscript
 temp = rt.selection[0]
 temp.name = "High_" + origal.name
 rt.addmodifier(temp,rt.Turbosmooth(iterations = 2,sepBySmGroups = True))
