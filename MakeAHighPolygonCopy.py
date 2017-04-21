53a445ad09c5a6e773beeb69fb04f39d1b6d4472
darthperry <yutengzhang87@gmail.com>
2017-04-20 23:58:27 -0400

    Add files via upload

--- MakeAHighPolygonCopy.py
+++ MakeAHighPolygonCopy.py
@@ -1,8 +1,13 @@
 from pymxs import runtime as rt
-origal = rt.selection[0]
-rt.execute("maxOps.cloneNodes $ cloneType:#copy newNodes:&nnl select nnl")
-# have to use Maxscript
-temp = rt.selection[0]
-temp.name = "High_" + origal.name
-rt.addmodifier(temp,rt.Turbosmooth(iterations = 2,sepBySmGroups = True))
-rt.addmodifier(temp,rt.Turbosmooth(iterations = 1))
+
+def main():
+	original = rt.selection[0]
+	rt.execute("maxOps.cloneNodes $ cloneType:#copy newNodes:&nnl select nnl")# have to use Maxscript
+	temp = rt.selection[0]
+	temp.name = "High_" + original.name
+	rt.addmodifier(temp,rt.Turbosmooth(iterations = 2,sepBySmGroups = True))
+	rt.addmodifier(temp,rt.Turbosmooth(iterations = 1))
+
+
+if __name__ == "__main__":
+	main()
