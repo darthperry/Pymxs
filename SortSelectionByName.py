d0170c178146db4907f2e39c7333aa372957d38d
darthperry <yutengzhang87@gmail.com>
2017-04-20 23:44:54 -0400

    Add files via upload

--- SortSelectionByName.py
+++ SortSelectionByName.py
@@ -1,14 +1,20 @@
 from pymxs import runtime as rt
-Temp = []
-ListResult = []
-x = 0
-for i in rt.selection:
-   Temp.append(str(i.name))
 
-Temp.sort()
-   
-for i in rt.selection:
-   ListResult.append(rt.getnodebyname(Temp[x]))
-   x += 1 #Python has no ++
-   
-del Temp
+ListResult = [] # User should get the name by them defined
+
+def main():
+	Temp = []
+	x = 0
+	for i in rt.selection:
+		Temp.append(str(i.name))
+
+	Temp.sort()
+	   
+	for i in rt.selection:
+		ListResult.append(rt.getnodebyname(Temp[x]))
+		x += 1 #Python has no ++
+	   
+	return ListResult	
+	
+if __name__ == "__main__":
+	main()
