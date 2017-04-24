c06497b437b2f7e9c565c2f90e1228da69478f55
darthperry <yutengzhang87@gmail.com>
2017-04-23 21:05:53 -0400

    Add files via upload

--- SortSelection.py
+++ SortSelection.py
@@ -14,11 +14,29 @@ def SortByName():
 	for i in rt.selection:
 		ListResult.append(rt.getnodebyname(Temp[x]))
 		x += 1 #Python has no ++
-	   
+	print ListResult    
 	return ListResult	
 	
 def SortByVolume():
-	Pass
+	ListResult = []
+	Temp = []
+	x = 0
+	def Volume(i):
+		volume = (i.max[0]-i.min[0])*(i.max[1]-i.min[1])*(i.max[2]-i.min[2])
+		#calculate volmue by BoundingBox
+		return volume
+		
+	for i in rt.selection:
+		result = [Volume(i),str(i.name)]		
+		Temp.append(result)
+
+	Temp.sort()
+		   
+	for i in rt.selection:
+		ListResult.append(rt.getnodebyname(Temp[x][1]))
+		x += 1 #Python has no ++
+	print ListResult   
+	return ListResult	
 	
 def SortByPolycount():
 	Pass
